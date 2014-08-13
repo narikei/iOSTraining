e71bc388f6990944f6feafa72ec0c09c9b19f557
なりたけいすけ <narikei1030@gmail.com>
2014-08-14 01:03:16 +0900

    obc HomeWork 1

--- HomeWorks/1.1/BasicObjectiveC/BasicObjectiveC/ViewController.m
+++ HomeWorks/1.1/BasicObjectiveC/BasicObjectiveC/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+static NSMutableArray *ques;
+
 @interface ViewController ()
 
 @end
@@ -19,8 +21,68 @@
     [super viewDidLoad];
 
     // TODO ここに課題を書き進めていってください
+    
+    // 1
+    NSDictionary *dict1 = @{
+                            @"domain": @"mixi.jp",
+                            @"entry":  @[@"list_voice.pl", @"list_diary.pl", @"list_mymall_item.pl"]
+                            };
+    NSDictionary *dict2 = @{
+                            @"domain": @"mmall.jp",
+                            @"entry":  @[@{
+                                            @"path":  @"add_diary.pl",
+                                             @"query": @{ @"tag_id": @"7"}
+                                        }]
+                            };
+    NSDictionary *dict3 = @{
+                            @"domain": @"itunes.apple.com",
+                            };
+    
+    
+    NSMutableArray *mutableArray = [NSMutableArray arrayWithArray:@[dict1, dict2]];
+    [mutableArray addObject:dict3];
+
+    //NSLog(@"%@", mutableArray);
 
 
+    // 2
+    ques = [NSMutableArray arrayWithArray:@[]];
+    [self push:@"1"];
+    [self push:@"2"];
+    [self push:@"3"];
+    [self push:@"4"];
+    [self push:@"5"];
+    
+    NSLog(@"count:%d", [self size]);
+    NSLog(@"pop:  %@", [self pop]);
+    NSLog(@"count:%d", [self size]);
+    NSLog(@"pop  :%@", [self pop]);
+    NSLog(@"count:%d", [self size]);
+    NSLog(@"pop  :%@", [self pop]);
+    NSLog(@"pop  :%@", [self pop]);
+    NSLog(@"pop  :%@", [self pop]);
+    NSLog(@"count:%d", [self size]);
+}
+
+- (void)push:(id)object
+{
+    [ques addObject:object];
+}
+
+- (id)pop
+{
+    if (ques.count == 0) {
+        return nil;
+    }
+    
+    id obj = ques[0];
+    [ques removeObjectAtIndex:0];
+    return obj;
+}
+
+- (NSInteger)size
+{
+    return ques.count;
 }
 
 - (void)didReceiveMemoryWarning
